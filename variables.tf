60b45d643149a9e6f1f881d93438fc0f69c26035
skatta3 <skatta3@yahoo.com>
2023-03-16 20:22:33 +0530

    Terraform Modules

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-0a84f2cba864e87d1"
+}
+variable "amiID" {
+	default="ami-0ba62214afa52bec7"
+}
+variable "keyPair" {
+	default="MyEC2Keys"
+}
+variable "keyPair1" {
+	default="MyEC2Keys.pem"
+}
+
+
+
+
